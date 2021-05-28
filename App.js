0e397e08b4a418fb7e70fd5821505d2463dda18c
Aanya <jainaanya2000@gmail.com>
2021-05-28 09:57:36 +0530

    changes

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import RNBootSplash from 'react-native-bootsplash';
 
 import AuthNavigation from './app/navigation/AuthNavigation';
 import AppBottomNavigation from './app/navigation/AppBottomNavigation';
+
+import Payment from './app/screens/Payment';
 
 import Profile from './app/screens/Profile';
 import LoginScreen from './app/screens/LoginScreen';
@@ -16,22 +18,23 @@ import DetailNavigation from './app/navigation/DetailNavigation';
 const Stack = createStackNavigator();
 function App() {
   useEffect(() => {
-    RNBootSplash.hide({duration: 250});
+    RNBootSplash.hide({ duration: 250 });
   }, []);
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{headerShown: false}}
-        initialRouteName="Auth">
-        <Stack.Screen name="Auth" component={AuthNavigation} />
-        <Stack.Screen name="AppBottom" component={AppBottomNavigation} />
-        <Stack.Screen name="Pay" component={PaymentNavigation} />
-        <Stack.Screen name="PayDetail" component={DetailNavigation} />
-        <Stack.Screen name="Profile" component={Profile} />
-        <Stack.Screen name="LoginPage" component={LoginScreen} />
-        <Stack.Screen name="RegisterPage" component={RegisterScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Payment />
+    // <NavigationContainer>
+    //   <Stack.Navigator
+    //     screenOptions={{headerShown: false}}
+    //     initialRouteName="Auth">
+    //     <Stack.Screen name="Auth" component={AuthNavigation} />
+    //     <Stack.Screen name="AppBottom" component={AppBottomNavigation} />
+    //     <Stack.Screen name="Pay" component={PaymentNavigation} />
+    //     <Stack.Screen name="PayDetail" component={DetailNavigation} />
+    //     <Stack.Screen name="Profile" component={Profile} />
+    //     <Stack.Screen name="LoginPage" component={LoginScreen} />
+    //     <Stack.Screen name="RegisterPage" component={RegisterScreen} />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
   );
 }
 
